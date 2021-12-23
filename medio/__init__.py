53c2aca83d5d26fd7b6b6592ae79ffdfae646463
Jacob Reinhold <reinhoj@mskcc.org>
2021-12-23 10:04:25 -0500

    Bump version: 0.1.2 → 0.1.3

--- medio/__init__.py
+++ medio/__init__.py
@@ -7,7 +7,7 @@ __description__ = "read arbitrary medical image formats in python"
 __url__ = "https://github.com/jcreinhold/pymedio"
 __author__ = """Jacob Reinhold"""
 __email__ = "jcreinhold@gmail.com"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021 Jacob Reinhold"
 
