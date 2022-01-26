fix bug in affine matrix calculation in dicom

medio/base.py
@@ -31,7 +31,7 @@ class ImageBase(np.ndarray):
     def __new__(
         cls: typing.Type[_Image],
         data: npt.ArrayLike,
-        affine: typing.Optional[npt.NDArray] = None,
+        affine: npt.NDArray | None = None,
     ) -> _Image:
         obj = cls._check_data(data).view(cls)
         obj._affine = cls._check_affine(affine)
@@ -64,13 +64,12 @@ class ImageBase(np.ndarray):
         *inputs: typing.Any,
         **kwargs: typing.Any,
     ) -> typing.Any:
-        """https://numpy.org/doc/stable/reference/generated/numpy.lib.mixins.NDArrayOperatorsMixin.html"""
         out = kwargs.get("out", ())
         for x in inputs + out:
             if not isinstance(x, self._HANDLED_TYPES + (self.__class__,)):
                 return NotImplemented
         affine = self.affine
-        is_cls = lambda y: isinstance(y, self.__class__)
+        is_cls = lambda y: isinstance(y, self.__class__)  # noqa: E731
         ufunc_args = tuple(x.view(np.ndarray) if is_cls(x) else x for x in inputs)
         if out:
             kwargs["out"] = tuple(x.view(np.ndarray) if is_cls(x) else x for x in out)
@@ -132,25 +131,23 @@ class ImageBase(np.ndarray):
         return data
 
     @staticmethod
-    def _check_affine(affine: typing.Optional[npt.NDArray]) -> npt.NDArray:
+    def _check_affine(affine: npt.NDArray | None) -> npt.NDArray:
         if affine is None:
-            return np.eye(4)
+            return np.eye(4, dtype=np.float64)
         if not isinstance(affine, np.ndarray):
             bad_type = type(affine)
             raise TypeError(f"Affine must be a NumPy array, not {bad_type}")
         if affine.shape != (4, 4):
             bad_shape = affine.shape
             raise ValueError(f"Affine shape must be (4, 4), not {bad_shape}")
-        return affine
+        return affine.astype(dtype=np.float64)
 
     def to_npz(self, file: miot.PathLike | typing.BinaryIO) -> None:
         np.savez_compressed(file, data=self.view(np.ndarray), affine=self.affine)
 
     @classmethod
     def from_npz(
-        cls: typing.Type[_Image],
-        file: miot.PathLike | typing.BinaryIO,
-        **np_load_kwargs,
+        cls, file: miot.PathLike | typing.BinaryIO, **np_load_kwargs
     ) -> _Image:
         _data = np.load(file, **np_load_kwargs)
         return cls(_data["data"], affine=_data["affine"])

medio/dicom.py
@@ -47,10 +47,11 @@ class Cosines:
 
     @classmethod
     def from_orientation(
-        cls: typing.Type[Cosines], image_orientation: typing.List[builtins.float]
+        cls: typing.Type[Cosines],
+        image_orientation: typing.Sequence[builtins.float] | npt.NDArray,
     ) -> Cosines:
-        row_cosine = np.asanyarray(image_orientation[:3])
-        column_cosine = np.asanyarray(image_orientation[3:])
+        row_cosine = np.asanyarray(image_orientation[:3], dtype=np.float64)
+        column_cosine = np.asanyarray(image_orientation[3:], dtype=np.float64)
         slice_cosine = np.cross(row_cosine, column_cosine)
         cosines = cls(row_cosine, column_cosine, slice_cosine)
         cosines.validate()
@@ -118,7 +119,9 @@ class SortedSlices:
     ) -> SortedSlices:
         """sort list of pydicom datasets into the correct order"""
         assert slice_datasets, "slice_datasets empty"
-        image_orientation = slice_datasets[0].ImageOrientationPatient
+        image_orientation = np.asanyarray(
+            slice_datasets[0].ImageOrientationPatient, dtype=np.float64
+        )
         cosines = Cosines.from_orientation(image_orientation)
         cs = cosines.slice
         positions = [np.dot(cs, d.ImagePositionPatient).item() for d in slice_datasets]
@@ -151,6 +154,10 @@ class SortedSlices:
                     warnings.warn(msg)
 
     @functools.cached_property
+    def patient_position(self) -> npt.NDArray:
+        return np.asanyarray(self.slices[0].ImagePositionPatient, dtype=np.float64)
+
+    @functools.cached_property
     def slice_spacing(self) -> builtins.float:
         spacing: builtins.float
         if len(self) > 1:
@@ -165,12 +172,15 @@ class SortedSlices:
     @functools.cached_property
     def affine(self) -> npt.NDArray:
         row_spacing, column_spacing = self.slices[0].PixelSpacing
-        transform = np.identity(4, dtype=np.float32)
+        transform = np.identity(4, dtype=np.float64)
+        slice_spacing = self.slice_spacing or 1.0
         transform[:3, 0] = self.cosines.row * column_spacing
         transform[:3, 1] = self.cosines.column * row_spacing
-        transform[:3, 2] = self.cosines.slice * self.slice_spacing
-        transform[:3, 3] = self.positions[0]
-        return transform
+        transform[:3, 2] = self.cosines.slice * slice_spacing
+        transform[:3, 3] = self.patient_position
+        _flipxy_44 = miou.flipxy_44()
+        transform_ras: npt.NDArray = np.dot(_flipxy_44, transform)
+        return transform_ras
 
 
 @dataclasses.dataclass(frozen=True)
@@ -237,7 +247,7 @@ class DICOMDir:
         elif (
             not isinstance(dicom_path, (builtins.str, pathlib.Path))
             and miou.is_iterable(dicom_path)
-            and all(str(p).endswith(".dcm") for p in dicom_path)  # type: ignore[union-attr]
+            and all(str(p).endswith(".dcm") for p in dicom_path)  # type: ignore[union-attr]  # noqa: E501
         ):
             paths = tuple(dicom_path)  # type: ignore[arg-type]
         else:

medio/utils.py
@@ -2,6 +2,8 @@
 Author: Jacob Reinhold <jcreinhold@gmail.com>
 """
 
+from __future__ import annotations
+
 __all__ = [
     "check_uint_to_int",
     "ensure_4d",
@@ -24,12 +26,12 @@ import medio.typing as miot
 
 # Matrices used to switch between LPS and RAS
 def flipxy_33() -> npt.NDArray:
-    matrix: npt.NDArray = np.diag([-1, -1, 1])
+    matrix: npt.NDArray = np.diag((-1.0, -1.0, 1.0)).astype(np.float64)
     return matrix
 
 
 def flipxy_44() -> npt.NDArray:
-    matrix: npt.NDArray = np.diag([-1, -1, 1, 1])
+    matrix: npt.NDArray = np.diag((-1.0, -1.0, 1.0, 1.0)).astype(np.float64)
     return matrix
 
 
@@ -92,7 +94,7 @@ def get_metadata_from_ras_affine(
 
 
 def ensure_4d(
-    array: npt.NDArray, *, num_spatial_dims: typing.Optional[builtins.int] = None
+    array: npt.NDArray, *, num_spatial_dims: builtins.int | None = None
 ) -> npt.NDArray:
     """for PyTorch"""
     num_dimensions = array.ndim

tests/test_medio.py
@@ -178,6 +178,14 @@ def test_dicomimage_from_path(dicom_image_dir: pathlib.Path) -> None:
     assert image.shape == (TEST_IMAGE_SHAPE + (NUM_DUPLICATES,))
 
 
+def test_affine_in_image_vs_dicomimage(dicom_image_dir: pathlib.Path) -> None:
+    dcm_image = miod.DICOMImage.from_path(dicom_image_dir)
+    image = mioi.Image.from_path(dicom_image_dir)
+    assert np.allclose(
+        dcm_image.affine, image.affine
+    ), f"\n{dcm_image.affine}\n\n{image.affine}"
+
+
 def test_dicomimage_from_zipped_stream(zipped_dicom_path: pathlib.Path) -> None:
     with open(zipped_dicom_path, "rb") as f:
         image = miod.DICOMImage.from_zipped_stream(f)
@@ -227,6 +235,7 @@ def test_nifti_gzipped_image_from_zipped_stream(
     assert image.shape == NIFTI_IMAGE_SHAPE
 
 
+# flake8: noqa: E501
 def test_numpy_ufuncs_on_dicom_image(dicom_image_dir: pathlib.Path) -> None:
     image = miod.DICOMImage.from_path(dicom_image_dir)
     assert image.shape == (TEST_IMAGE_SHAPE + (NUM_DUPLICATES,))
@@ -234,7 +243,7 @@ def test_numpy_ufuncs_on_dicom_image(dicom_image_dir: pathlib.Path) -> None:
     image += 1.0
     image *= image
     assert isinstance(image, miod.DICOMImage)
-    s = f"DICOMImage(shape: (128, 128, {NUM_DUPLICATES}); spacing: (0.66, 0.66, 0.00); dtype: float32)"
+    s = f"DICOMImage(shape: (128, 128, {NUM_DUPLICATES}); spacing: (0.66, 0.66, 1.00); dtype: float32)"
     assert str(image) == s
     mask = image == 0.0
     assert isinstance(mask, miod.DICOMImage)
@@ -242,10 +251,11 @@ def test_numpy_ufuncs_on_dicom_image(dicom_image_dir: pathlib.Path) -> None:
     assert isinstance(subimage, miod.DICOMImage)
     image = image.astype(np.float16)
     assert isinstance(image, miod.DICOMImage)
-    s = f"DICOMImage(shape: (128, 128, {NUM_DUPLICATES}); spacing: (0.66, 0.66, 0.00); dtype: float16)"
+    s = f"DICOMImage(shape: (128, 128, {NUM_DUPLICATES}); spacing: (0.66, 0.66, 1.00); dtype: float16)"
     assert str(image) == s
 
 
+# flake8: noqa: E501
 def test_numpy_ufuncs_on_image(image: mioi.Image) -> None:
     image += 1.0
     mask = image == 1.0
@@ -268,6 +278,7 @@ def test_numpy_ufuncs_on_image(image: mioi.Image) -> None:
     assert isinstance(image, mioi.Image)
     _image2 = image.view(np.ndarray)
     assert isinstance(image, np.ndarray)
+    assert isinstance(_image2, np.ndarray)
 
 
 @pytest.mark.skipif(torch is None, reason="Requires torch")