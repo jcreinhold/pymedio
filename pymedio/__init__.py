4953cdbde0b129bc1a9f8db7463da7a534a2e32f
Jacob Reinhold <5241441+jcreinhold@users.noreply.github.com>
2022-01-27 18:08:07 -0500

    Bump version: 0.1.9 → 0.2.0

--- pymedio/__init__.py
+++ pymedio/__init__.py
@@ -7,7 +7,7 @@ __description__ = "read arbitrary medical image formats in python"
 __url__ = "https://github.com/jcreinhold/pymedio"
 __author__ = """Jacob Reinhold"""
 __email__ = "jcreinhold@gmail.com"
-__version__ = "0.1.9"
+__version__ = "0.2.0"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021 Jacob Reinhold"
 
