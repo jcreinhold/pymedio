eace0c8602ae70d07aa83a8ae2ade093fccdb983
Jacob Reinhold <5241441+jcreinhold@users.noreply.github.com>
2022-03-11 12:20:27 -0500

    clean up resample implementation

--- pymedio/base.py
+++ pymedio/base.py
@@ -225,15 +225,14 @@ class BasicImage(npt.NDArray[miot.DType]):
 
             data = np.apply_along_axis(interpolate, i, data)
         # https://math.stackexchange.com/q/1120209
-        # TODO: verify this is correct
         rzs = self.affine[:3, :3]  # rotation, scale (zoom), shear
         rotation, zs = np.linalg.qr(rzs)
-        scale = np.diag(zs.diagonal())
-        shear = np.linalg.inv(scale) @ zs
+        scale = zs.diagonal()
+        shear = zs * (1.0 / scale)
         new_affine = np.zeros_like(self.affine)
-        new_scale = scale @ np.diag(resolution_scale)
+        new_scale = scale * resolution_scale
         new_affine[:3, :3] = (rotation * new_scale) @ shear
-        new_affine[:3, 3] = self.affine[:3, 3]
+        new_affine[:, 3] = self.affine[:, 3]
         return self.__class__(
             data.astype(self.dtype, copy=False), new_affine, info=self.info
         )
